06ee55070d071b3043d4bd7aec7073902edeea0d
Tapavinbal <daran.nezet@free.fr>
2020-12-30 11:52:42 +0100

    ON VA MANGER, C'EST BON!

--- serveur.hh
+++ serveur.hh
@@ -5,6 +5,12 @@
 class Serveur : public Personnel{
 	public:
 		virtual static size_t i;
+		Serveur(string n, string p, int h);
+		std::string getNom() const {return nom;}
+		std::string getPrenom() const {return prenom;}
+		std::int getRegime() const {return regime;}
+		std::size_t getId() const {return id_serveur;}
 	private:
+		std::size_t id_serveur;
 		std::vector<Table> tables;		//Le numéro des tables attitrées
 }
